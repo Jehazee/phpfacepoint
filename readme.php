2086ad80d61b3ed8446e37bc142616fa830d5ca8
Algahazi Abdulrahman <34718923+Jehazee@users.noreply.github.com>
2018-10-11 15:07:23 +0400

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+Once the application will be ready to lunch and usable for endusers and front-end developers 
+i'll share it on GitHub
+Contact Details: algahazi.abdulrahman@gmail.com
+*/
+
+?>
